e2cdca14fb55659656a70bae26870dc727357625
Arash <arash8877@gmail.com>
2024-05-23 09:34:50 +0200

    modified HangmanWord

--- src/components/HangmanWord.tsx
+++ src/components/HangmanWord.tsx
@@ -1,7 +1,38 @@
-import React from "react";
-
 const HangmanWord = () => {
-  return <div>HangmanWord</div>;
+  const word = "test";
+  const guessedLetters = ["t", "s", "e"];
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        gap: "0.25em",
+        fontSize: "6rem",
+        fontWeight: "bold",
+        textTransform: "uppercase",
+        fontFamily: "monospace",
+      }}
+    >
+      {word.split("").map((letter, index) => (
+        <span
+          style={{
+            borderBottom: "0.1em solid black",
+          }}
+          key={index}
+        >
+          <span
+            style={{
+              visibility: guessedLetters.includes(letter)
+                ? "visible"
+                : "hidden",
+            }}
+          >
+            {letter}
+          </span>
+        </span>
+      ))}
+    </div>
+  );
 };
 
 export default HangmanWord;
